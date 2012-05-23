1e6df33d1424305f7a950e2da717594f7e4b113e
Marian Sievers <marian.sievers@gmail.com>
2012-05-23 20:34:21 +0200

    Basic sketch for Startpage

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<html><head><title>InstaNode</title></head><body>");
+  res.write("<h1>This is InstaNode</h1>");
+  res.write('<p>Just another Instagramm clone, written with the power of Node.js</p>');
+  res.end("</body></html>");
 }).listen(8124, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:8124/');
